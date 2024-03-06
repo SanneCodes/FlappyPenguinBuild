37c732f3da856645ad68c8b8a6a7268d785496d7
SanneCodes <codessand@gmail.com>
2024-03-06 20:22:46 +0100

    Fixed typing error in script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ cd /var/www
 cd html
 cd Build/
 sudo brotli --decompress FlappyPenguinBuild.data.br FlappyPenguinBuild.framework.js.br FlappyPenguinBuild.wasm.br
-sudo rm -v FlappyPenguinBuild.br FlappyPenguinBuild.js.br FlappyPenguinBuild.wasm.br
+sudo rm -v FlappyPenguinBuild.data.br FlappyPenguinBuild.framework.js.br FlappyPenguinBuild.wasm.br
 sudo mv FlappyPenguinBuild.data FlappyPenguinBuild.data.br
 sudo mv FlappyPenguinBuild.framework.js FlappyPenguinBuild.framework.js.br
 sudo mv FlappyPenguinBuild.wasm FlappyPenguinBuild.wasm.br
